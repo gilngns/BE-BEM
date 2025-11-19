b0deca066ab1633bfdc7fd7af7a287134d1adcdf
gilngns <nsgilang62@gmail.com>
2025-11-19 15:01:35 +0700

    perbaikan ke 3 untuk endpoint hasil prediksi

--- src/routes/prediksiRoutes.js
+++ src/routes/prediksiRoutes.js
@@ -1,15 +1,9 @@
-import { Router } from "express";
-import {
-  createPencacahan,
-  getAllPencacahan,
-  getPencacahanById,
-  updatePencacahan,
-  deletePencacahan,
-} from "../controllers/pencacahanController.js";
-import { getPrediksiBySiklus } from "../controllers/siklusController.js";
+import express from "express";
+import { getPrediksiBySiklus } from "../controllers/prediksiController.js";
 import { authMiddleware } from "../authMiddleware.js";
 
-const router = Router();
+const router = express.Router();
+
 /**
  * @swagger
  * /api/prediksi/siklus/{siklusId}:
@@ -29,10 +23,5 @@ const router = Router();
  *         description: List prediksi berhasil diambil
  */
 router.get("/siklus/:siklusId", authMiddleware, getPrediksiBySiklus);
-router.post("/", createPencacahan);
-router.get("/", getAllPencacahan);
-router.get("/:id", getPencacahanById);
-router.put("/:id", updatePencacahan);
-router.delete("/:id", deletePencacahan);
 
 export default router;
